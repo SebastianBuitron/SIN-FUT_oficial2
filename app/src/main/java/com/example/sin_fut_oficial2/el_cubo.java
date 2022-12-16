c844bba647f15ee9462344df7e3a6c84e3f971f4
SebastianBuitron <sebastbuitroon@unicauca.edu.co>
2022-12-16 13:57:49 -0500

    fragmentos

--- app/src/main/java/com/example/sin_fut_oficial2/el_cubo.java
+++ app/src/main/java/com/example/sin_fut_oficial2/el_cubo.java
@@ -2,7 +2,9 @@ package com.example.sin_fut_oficial2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.ViewFlipper;
 
@@ -33,6 +35,12 @@ public class el_cubo extends AppCompatActivity  {
         g_cubo.setInAnimation(this, android.R.anim.slide_out_right);
         g_cubo.setOutAnimation(this, android.R.anim.slide_out_right);
 
+
+    }
+    public void Ver_metodopago(View view) {
+
+        Intent ver = new Intent(this, metodopago.class);
+        startActivity(ver);
 
     }
 
---
+++ app/src/main/java/com/example/sin_fut_oficial2/metodopago.java
@@ -0,0 +1,40 @@
+package com.example.sin_fut_oficial2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class metodopago extends AppCompatActivity {
+
+    FragmentTransaction transaction;
+    Fragment fragmentincial, fragment_banco, fragment_paypal;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_metodopago);
+
+        //fragmentincial= new metodopago();
+        fragment_banco=new banco();
+        fragment_paypal=new paypal();
+
+
+        getSupportFragmentManager().beginTransaction().add(R.id.fragmentinicio,fragment_banco).commit ();
+
+    }
+    public void onClick (View view){
+        transaction=getSupportFragmentManager().beginTransaction();
+        switch (view.getId()) {
+            case R.id.banco: transaction.replace(R.id.fragmentinicio, fragment_banco);
+                transaction.addToBackStack(null);
+                break;
+            case R.id.paypal:transaction.replace(R.id.fragmentinicio, fragment_paypal);
+                transaction.addToBackStack(null);
+                break;
+        }
+        transaction.commit();
+    }
+}
